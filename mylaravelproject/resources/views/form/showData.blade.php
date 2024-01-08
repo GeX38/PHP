ba02b8c2f4bbceb97646adfe81bdb222d03faa04
GeX38 <murzint@gmail.com>
2024-01-08 16:07:26 +0800

    Correcting table in showData

--- mylaravelproject/resources/views/form/showData.blade.php
+++ mylaravelproject/resources/views/form/showData.blade.php
@@ -6,20 +6,20 @@
     <h1>Data Page</h1>
 
     @if(count($formData) > 0)
-        <table>
+        <table style="border-collapse: collapse; width: 100%;">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Message</th>
+                    <th style="border: 1px solid #ddd; padding: 8px;">Name</th>
+                    <th style="border: 1px solid #ddd; padding: 8px;">Email</th>
+                    <th style="border: 1px solid #ddd; padding: 8px;">Message</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($formData as $data)
                     <tr>
-                        <td>{{ $data['name'] }}</td>
-                        <td>{{ $data['email'] }}</td>
-                        <td>{{ $data['message'] }}</td>
+                        <td style="border: 1px solid #ddd; padding: 8px;">{{ $data['name'] }}</td>
+                        <td style="border: 1px solid #ddd; padding: 8px;">{{ $data['email'] }}</td>
+                        <td style="border: 1px solid #ddd; padding: 8px;">{{ $data['message'] }}</td>
                     </tr>
                 @endforeach
             </tbody>
