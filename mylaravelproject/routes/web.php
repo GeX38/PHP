43e719ec8b19b4cc1b54c9c36a387928fc77f798
GeX38 <murzint@gmail.com>
2024-01-08 14:47:32 +0800

    Update web.php

--- mylaravelproject/routes/web.php
+++ mylaravelproject/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FormController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/form', [FormController::class, 'showForm']);
+Route::post('/form', [FormController::class, 'processForm']);
+
+Route::get('/data', [FormController::class, 'showData']);
