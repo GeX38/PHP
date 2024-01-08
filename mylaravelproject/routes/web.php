861e0961ff4c0424ce7f68583758c1ebce6fa73f
GeX38 <murzint@gmail.com>
2024-01-09 02:40:17 +0800

    Updating routes

--- mylaravelproject/routes/web.php
+++ mylaravelproject/routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/form', [FormController::class, 'showForm']);
 Route::post('/form', [FormController::class, 'processForm']);
-
 Route::get('/data', [FormController::class, 'showData']);
+
+Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
